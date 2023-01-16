0eda0f340bdb8449f136d7d0d20ebe03fdb87b0b
WilmanMorocho <117868799+WilmanMorocho@users.noreply.github.com>
2023-01-16 17:24:45 -0500

    Add files via upload

---
+++ implementacion/InfoDeAutor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evaluacion2u;
+
+/**
+ *
+ * @author Usuario Pc
+ */
+public class InfoDeAutor {
+    private String descripcion;
+    private String nombre;
+    
+    public static void mostrarInformacion(String descripcion, String nombre){
+    
+    }
+}
---
+++ implementacion/PelisWeb.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evaluacion2u;
+
+/**
+ *
+ * @author Usuario Pc
+ */
+public class PelisWeb {
+
+    private String peliculaList;
+    private String pestañaList;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
